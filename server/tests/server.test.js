ead793862a2a8e3ff324aed07c69981c65ecb315
Matthew Kuhn <Matthew.Kuhn@fmr.com>
2018-01-15 15:05:57 -0500

    Add tests for delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -94,3 +94,41 @@ describe('GET /todos/:id', () => {
     .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove a todo', (done) => {
+    request(app)
+    .delete(`/todos/${todos[1]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(todos[1]._id.toHexString());
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.findById(todos[1]._id.toHexString()).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => {
+        done(e);
+      });
+    });
+  });
+
+  it('Should return a 404 if todo not found', (done) => {
+    request(app)
+    .delete(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('Should return a 404 if todo id is invalid', (done) => {
+    request(app)
+    .delete(`/todos/123`)
+    .expect(404)
+    .end(done);
+  });
+
+});
